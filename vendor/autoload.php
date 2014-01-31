0d8c63cbbad8c0e61e2d6cf336710f1f5a051f1e
Mahendri Winata <mahen.0112@gmail.com>
2014-02-01 00:28:40 +0700

    add session config and database

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInit075d201e5727a308e667d14bbb6dde19::getLoader();
+return ComposerAutoloaderInit36faa6313d9b906efd85f3f2031cbf70::getLoader();
